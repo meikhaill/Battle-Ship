9930582825a845cd6f58c0d153e12ece0bc7a05e
meikhaill <meikhailmajor9@gmail.com>
2024-03-04 03:10:11 -0700

    Iteration 1 complete

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,55 @@
 class Cell
-  attr_accessor :coordinate
+  attr_accessor :coordinate, :empty
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+    @empty = true
+    @fired = false
   end
 
   def ship
-    unless @cell.include?(ship)
-      return false
+    @ship
+  end
+
+  def empty?
+    @empty
+  end
+
+  def place_ship(ship)
+    @ship = ship
+    @empty = false
+    @fired = false
+    return ship
+  end
+
+  def fired_upon?
+    @fired
+  end
+
+  def fire_upon
+    if @ship
+      @ship.hit
+      @fired = true
+      @ship
+    end
+    @fired = true
+  end
+
+  def render(show_ship = false)
+    if self.fired_upon? == true && @empty == false && @ship.health == 0
+      return "X"
+    elsif self.fired_upon? == true && @empty == false
+      return "H"
+    elsif self.fired_upon? == true && @empty == true
+      return "M"
+    elsif show_ship && @ship
+      return "S"
+    else 
+      return "."
     end
   end
+
+
+
+
 end
