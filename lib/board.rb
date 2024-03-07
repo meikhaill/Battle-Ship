e93e1ddbed68e4d97843b02afd43d48c5f44cedc
meikhaill <meikhailmajor9@gmail.com>
2024-03-07 13:03:55 -0700

    WIP fixing board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
 class Board 
   attr_accessor :cells
   def initialize
+    @horizontal = false
+    @vertical = false
     @cells = {}
     ('A'..'D').each do |row|
       (1..4).each do |col|
@@ -19,13 +21,31 @@ class Board
   end
 
   def valid_placement?(ship, coords)
-    
-    if ship.length == coords.size
-      @cells.keys.each_cons(2).all? do |a, b| 
-        if b == a + 1
-          return true
-        end
-      end
+    #gpt response
+    return false if coords.size != ship.length
+
+    numeric_coords = coords.map { |coord| [coord[0].ord - 'A'.ord + 1, coord[1].to_i] }
+    horizontal = numeric_coords.map(&:last).uniq.size == 1
+    vertical = numeric_coords.map(&:first).uniq.size == 1
+
+    return false unless horizontal || vertical
+
+    sorted_coords = horizontal ? numeric_coords.sort_by { |coord| coord[1] } : numeric_coords.sort_by { |coord| coord[0] }
+
+    continuity_check = sorted_coords.each_cons(2).all? do |a, b|
+      horizontal ? (b[1] - a[1] == 1) : (b[0] - a[0] == 1)
     end
+
+    return false unless continuity_check
+
+    return false if numeric_coords.any? { |x, y| x < 1 || x > 4 || y < 1 || y > 4 }
+
+    coords.each do |coordinate|
+      return false if @cells[coordinate] && @cells[coordinate].ship
+    end
+
+
+    true
   end
+
 end
