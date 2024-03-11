2f53f063c5f30dc27e13eca77a41da04882620cb
meikhaill <meikhailmajor9@gmail.com>
2024-03-11 11:10:54 -0600

    Added board rendering

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+require_relative 'board.rb'
 class Board 
   attr_accessor :cells
   def initialize
@@ -8,12 +9,19 @@ class Board
     ('A'..'D').each do |row|
       (1..4).each do |col|
         coordinate = "#{row}#{col}"
-        @cells[coordinate] = Cell.new(coordinate)
+        @cellsbo[coordinate] = Cell.new(coordinate)
       end
     end
   end
 
-  def place(coords)
+  def place(ship, coords)
+    return false unless valid_placement?(ship, coords)
+    
+    coords.each do |coordinate|
+      cell = @cells[coordinate]
+      cell.place_ship(ship)
+    end
+    true
     
   end 
 
@@ -33,20 +41,17 @@ class Board
       cell = @cells[coordinate]
       cell.fire_upon
     else
-      # Handle the case where the coordinate is not valid
-      # For example, you could raise an error or return a specific value
       raise "Invalid coordinate: #{coordinate}"
     end
  end
 
-  def render_check
-    stat = cell.render
-  "  1 2 3 4 \n" +
-  "A . . . . \n" +
-  "B . . . . \n" +
-  "C . . . . \n" +
-  "D . . . . \n"
-  end
+ 
+  # "  1 2 3 4 \n" +
+  # "A . . . . \n" +
+  # "B . . . . \n" +
+  # "C . . . . \n" +
+  # "D . . . . \n"
+  
 
   def valid_coordinate?(coordinate)
     if @cells.include?(coordinate)
@@ -56,45 +61,37 @@ class Board
     end
   end
 
-  def render_all
-    @cells.each do |cell|
-
-      status = i.render
-      print status
-    end
+   def render(show_ships = false)
+    board_header = "  1 2 3 4 \n"
+    board_rows = ['A', 'B', 'C', 'D'].map do |row|
+      row_content = (1..4).map do |col|
+        @cells["#{row}#{col}"].render(show_ships)
+      end.join(' ')
+      "#{row} #{row_content} \n"
+    end.join('')
+    board_header + board_rows
   end
 #everything above this is all fine and dandy, same with the other classes
  
   def valid_placement?(ship, coords)
-    #from here down is gpt response based off of me pasting my code, repasting gpts new code and editing it
-    
-    return false if coords.size != ship.length
-    #checking to make sure the amount of coords = length of the ship
-    
-    numeric_coords = coords.map { |coord| [coord[0].ord - 'A'.ord + 1, coord[1].to_i] }
-    #coord[0].ord - 'A'.ord is just a creative way of turning it into a value 0-3
-    #if the ord of index 1 is B then thats something like 98-97 which equates to 1 same as the index
-    
-    horizontal = numeric_coords.map(&:last).uniq.size == 1
-    vertical = numeric_coords.map(&:first).uniq.size == 1
-    #this just checks for duplicates split up by the array being first subarray being last
-    
-    return false unless horizontal == true && vertical == true
-    
-    sorted_coords = horizontal ? numeric_coords.sort_by { |coord| coord[1] } : numeric_coords.sort_by { |coord| coord[0] }
-   
-    continuity_check = sorted_coords.each_cons(2).all? do |a, b|
-      horizontal ? (b[1] - a[1] == 1) : (b[0] - a[0] == 1)
-    end
-
-    return false unless continuity_check
-    return false if numeric_coords.any? { |x, y| x < 1 || x > 4 || y < 1 || y > 4 }
-
-    coords.each do |coordinate|
-      return false if @cells[coordinate] && @cells[coordinate].ship
-    end
-
-    true
+     return false unless coords.size == ship.length
+  
+  # Extract rows and columns from coordinates
+  rows = coords.map { |coord| coord[0].ord - 'A'.ord }
+  cols = coords.map { |coord| coord[1..-1].to_i - 1 }
+  
+  # Check for linear and consecutive placement
+  linear_horizontal = rows.uniq.size == 1 && cols.each_cons(2).all? { |a, b| b == a + 1 }
+  linear_vertical = cols.uniq.size == 1 && rows.each_cons(2).all? { |a, b| b == a + 1 }
+  
+  # Ensure placement is either linear or valid reverse, not diagonal
+  valid_placement = linear_horizontal || linear_vertical
+  
+  return false unless valid_placement
+  
+  # Further checks for overlapping or out-of-bounds placement can be added here
+  
+  true
   end
 
 end
